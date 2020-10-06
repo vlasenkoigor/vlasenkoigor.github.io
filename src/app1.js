e606627aac6af8afac8fe4c79460dfb188cf60c4
igorv <1234567$it>
2020-10-06 11:39:19 +0300

    visibility change logs

--- src/app1.js
+++ src/app1.js
@@ -8,7 +8,12 @@ const width = 1280 , height = 720;
 // loader.load(()=>{
 //     startApp();
 // })
+document.addEventListener("visibilitychange", function() {
+    console.log(document.visibilityState);
+    addLog('visibilitychange + ' + document.visibilityState)
+});
 
+let logText = null;
 export default {
     start : (openApp2 = ()=>{})=>{
 
@@ -27,34 +32,21 @@ export default {
 
         const {stage, loader} = app;
 
-        const text = new PIXI.Text('This is APP 1', {fontSize : 80, fill : "#ffffff", fontWeight : "bolder" });
-        text.anchor.set(0.5);
+        const text = new PIXI.Text('Visibility change logs', {fontSize : 18, fill : "#ffffff", fontWeight : "bolder", align : 'center'});
+        text.anchor.set(0.5, 0);
 
         text.x = width / 2;
-        text.y = height / 2;
+        text.y = 20;
+
+        logText = text;
 
         stage.addChild(text);
-
-
-        const button = new PIXI.Graphics();
-        button.beginFill(0x603100);
-        button.drawRoundedRect(-100,-35,200, 70);
-        button.endFill();
-        button.x = width / 2;
-        button.y = height * 0.7
-        stage.addChild(button);
-
-        const buttonText = new PIXI.Text('Open app 2', {fontSize : 30, fill : "#ffffff", fontWeight : "bolder" })
-        buttonText.x = width / 2;
-        buttonText.y = height * 0.7
-        buttonText.anchor.set(0.5);
-        stage.addChild(buttonText);
-
-        button.interactive = true;
-        button.buttonMode = true;
-        button.on('click', openApp2);
-        button.on('tap', openApp2);
 
     }
 }
 
+
+
+function addLog(text) {
+    logText.text += '\n - Visibility change ' + text + ' ' + new Date().toLocaleString()
+}
